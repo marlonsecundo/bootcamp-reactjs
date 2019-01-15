687f9f75b9b3334da41dd9627b25bdd64c7fc127
Marlon Secundo <marlon_secundo@outlook.com>
2019-01-15 12:13:01 -0300

    First Component

--- module1/src/index.js
+++ module1/src/index.js
@@ -1,1 +1,21 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
+import { render } from "react-dom";
+
+class Button extends React.Component {
+  render() {
+    return <a href="">Salvar</a>;
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hellou</h1>
+        <Button />
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
