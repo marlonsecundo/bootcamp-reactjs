7978038e4d7882c0348978354e0e198f3bd53ab1
Marlon Secundo <marlon_secundo@outlook.com>
2019-01-16 15:03:03 -0300

    Repos list

---
+++ module2/src/pages/Main/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+import { Container, Form } from './styles';
+
+import CompareList from '../../components/CompareList';
+
+const Main = () => (
+  <Container>
+    <img src={logo} alt="Github Compare" />
+
+    <Form>
+      <input type="text" placeholder="usuario/repositorio" />
+      <button type="submit">OK</button>
+    </Form>
+
+    <CompareList />
+  </Container>
+);
+
+export default Main;
