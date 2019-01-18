e96cf96cd57c25f7d2956ab36ec9ea8607cc145c
Marlon Secundo <marlon_secundo@outlook.com>
2019-01-18 10:37:38 -0300

    Reducers

--- module3/src/App.js
+++ module3/src/App.js
@@ -3,9 +3,11 @@ import { Provider } from 'react-redux';
 
 import store from './store';
 
+import TodoList from './components/TodoList';
+
 const App = () => (
   <Provider store={store}>
-    <h1>Hello World</h1>
+    <TodoList />
   </Provider>
 );
 
