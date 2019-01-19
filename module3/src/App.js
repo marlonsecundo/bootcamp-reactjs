279103a7e32943dd437c253aea193080e318d8bd
Marlon Secundo <marlon_secundo@outlook.com>
2019-01-19 14:44:45 -0300

    Reactotron

--- module3/src/App.js
+++ module3/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
+import './config/reactotron';
+
 import store from './store';
 
 import TodoList from './components/TodoList';
