b7bb4c531971c292dc210aa060c78720b9bf132b
Marlon Secundo <marlon_secundo@outlook.com>
2019-01-19 16:28:08 -0300

    Interface

--- module3/src/pages/main/index.js
+++ module3/src/pages/main/index.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-const Main = () => <h1>Pagina 1</h1>;
+export default class Main extends React.Component {
+  state = {
+    repositoryInput: '',
+  };
 
-export default Main;
+  render() {
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            placeholder="usuario/repositorio"
+            value={this.state.repositoryInput}
+            onChange={(e) => {
+              this.setState({ repositoryInput: e.target.value });
+            }}
+          />
+          <button type="submit">Adicionar</button>
+          <ul>
+            <li>
+              <p>
+                <strong>facebook/react</strong> (descricao descricao descricao descricao descricao
+                descricao descricao descricao descricao descricao descricao descricao descricao
+                descricao)
+              </p>
+              <a href="http://github.com/facebook/react">Acessar</a>
+            </li>
+          </ul>
+        </form>
+      </Fragment>
+    );
+  }
+}
