d1771a261af7d429bd72514c88e2d4f09fe124ef
Marlon Secundo <marlon_secundo@outlook.com>
2019-01-20 11:20:50 -0300

    Footer

---
+++ module3/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const Footer = ({ count }) => <p>Tem {count} favoritos</p>;
+
+Footer.propTypes = {
+  count: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = state => ({
+  count: state.favorites.lenght,
+});
+
+export default connect(mapStateToProps)(Footer);
