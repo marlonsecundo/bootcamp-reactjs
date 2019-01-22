f7a312e89207d9d80c4e0b46aa03de6cf9ef6c63
Marlon Secundo <marlon_secundo@outlook.com>
2019-01-22 11:46:30 -0300

    Saga - select

--- module3/src/store/sagas/favorites.js
+++ module3/src/store/sagas/favorites.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import api from '../../services/api';
 
 import { Creators as FavoriteActions } from '../ducks/favorites';
@@ -6,6 +6,13 @@ import { Creators as FavoriteActions } from '../ducks/favorites';
 export function* addFavorite(action) {
   try {
     const { data } = yield call(api.get, `/repos/${action.payload.repository}`);
+
+    const isDuplicated = yield select(state => state.favorites.data.find(favorite => favorite.id === data.id));
+
+    if (isDuplicated) {
+      yield put(FavoriteActions.addFavoriteFailure('Erro ao adicionar repositorio'));
+      return;
+    }
 
     const repositoryData = {
       id: data.id,
